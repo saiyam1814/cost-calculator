6ee3395f05d38813c53c26352aaf97fa653d3502
Saiyam Pathak <saiyam911@gmail.com>
2023-01-15 18:01:19 +0530

    deploy to k8s

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+module.exports = {
+  output: 'standalone',
+}
